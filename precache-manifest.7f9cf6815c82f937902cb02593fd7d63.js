ff2b8336db601037dd568b5e8f39e05aaae5f604
Jayash Raj Mudbhari <jayashraj@outlook.com>
2020-07-01 21:57:51 +1000

    Updates

---
+++ precache-manifest.7f9cf6815c82f937902cb02593fd7d63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bdeb095703bd91647f4ce351d382c8",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "36109e46bb2049657176",
+    "url": "/quotes/static/css/main.ad5acf6c.chunk.css"
+  },
+  {
+    "revision": "628029eb594a645cefad",
+    "url": "/quotes/static/js/2.7bb95990.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes/static/js/2.7bb95990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36109e46bb2049657176",
+    "url": "/quotes/static/js/main.beb6b458.chunk.js"
+  },
+  {
+    "revision": "42135f8f194da03e2725",
+    "url": "/quotes/static/js/runtime-main.8b2ab2e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quotes/static/media/logo.5d5d9eef.svg"
+  }
+]);
